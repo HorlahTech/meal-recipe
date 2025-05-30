e4032ac941cdd65c39c81d02fced9bd291b04f8f
Ziko67890 <133816931+Ziko67890@users.noreply.github.com>
2025-05-29 18:50:39 -0700

    latest update

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -11,7 +11,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green.shade700,
+      // backgroundColor: Colors.green.shade700,
       appBar: AppBar(backgroundColor: Colors.green.shade700),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.green.shade700,
@@ -34,16 +34,99 @@ class _HomeScreenState extends State<HomeScreen> {
               children: [
                 Container(
                   height: 400,
-                  width: 400,
+                  width: double.infinity,
+                  clipBehavior: Clip.antiAlias,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(15),
+                    ),
                   ),
                   child: Image.asset('asset/jollof.png'),
                 ),
-                //  Text(
-                //   'Jollof Rice ',
-                //   style: TextStyle(fontSize: 28, color: Colors.green.shade700),
-                //  ),
+                SizedBox(height: 15),
+                Text(
+                  'Jollof Rice ',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.green.shade700,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Main Dish",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.green.shade700,
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    Text(
+                      '. West Africa',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.green.shade700,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Ingredient',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green.shade700,
+                  ),
+                ),
+                SizedBox(height: 15),
+                Text(
+                  '. 2 cups long grain parboiled rice\n . 4 large tomatoes (blended)\n . 2 red bell peppers\n . 1 scotch bonnet (optional for heat)',
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Instruction',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green.shade700,
+                  ),
+                ),
+                SizedBox(height: 15),
+                Text(
+                  'Heat oil and saute onion and pepper\n then stir in tomato sauce and rice',
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    GestureDetector(
+                      onLongPress: () {},
+                      child: Container(
+                        height: 120,
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.vertical(
+                            top: Radius.circular(16),
+                          ),
+                        ),
+                        child: Image.asset("asset/nija jollof.jpeg"),
+                      ),
+                    ),
+                    SizedBox(width: 28),
+                    Text(
+                      'How To Cook \nJollof Rice',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
